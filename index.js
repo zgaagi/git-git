4e3e4f39a955763cbcc99953fdd19e3ccc5ea8a5
Andrei Buber <zgaagi.bu@yandex.ru>
2020-08-04 21:27:43 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("The lesson one");
+
+// alert("hello")
+let age = prompt ("сколько тебе лет");
+console.log("age", age);
+let isBoss = confirm("Ты здесь главный?");
+console.log("isBoss", isBoss);
